088c2109c24d8853c41ae6a535c2e17774e294b7
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-11-02 20:30:50 +0100

    new sprint

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import pizzaData from "./data";
+
+console.log(pizzaData);
 function App() {
   let names = "Daniel and David";
   const message1 = "We are currently open ";
@@ -5,17 +8,35 @@ function App() {
   const hour = new Date().getHours();
   const openHour = 6;
   const closeHour = 17;
+  const isOpen = hour >= openHour && hour <= closeHour;
   return (
     <div>
       <h1>{new Date().toLocaleTimeString()}</h1>
       <h3>{names}</h3>
-      {hour >= openHour && hour <= closeHour ? (
-        <h1>{message1}</h1>
-      ) : (
-        <h1>{message2}</h1>
-      )}
+      {isOpen ? <h1>{message1}</h1> : <h1>{message2}</h1>}
+      {pizzaData.map((item) => {
+        return (
+          <Card
+            key={item.name}
+            name={item.name}
+            image={item.photoName}
+            ingredients={item.ingredients}
+          />
+        );
+      })}
     </div>
   );
 }
 
 export default App;
+
+const Card = (props) => {
+  return (
+    <div>
+      <img src={props.image} alt={props.name} />
+      <h3> {props.name} </h3>
+      <h1>{props.price}</h1>
+      <h1>{props.ingredients}</h1>
+    </div>
+  );
+};
