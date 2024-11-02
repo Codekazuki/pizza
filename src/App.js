15172ddb2662f3a2fa9ca7fe22b186a6b1c89a84
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-11-02 06:37:04 +0100

    new sprint

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 function App() {
   let names = "Daniel and David";
+  const message1 = "We are currently open ";
+  const message2 = "We are currently closed";
+  const hour = new Date().getHours();
+  const openHour = 6;
+  const closeHour = 17;
   return (
     <div>
       <h1>{new Date().toLocaleTimeString()}</h1>
-
       <h3>{names}</h3>
+      {hour >= openHour && hour <= closeHour ? (
+        <h1>{message1}</h1>
+      ) : (
+        <h1>{message2}</h1>
+      )}
     </div>
   );
 }
