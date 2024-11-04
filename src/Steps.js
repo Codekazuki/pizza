0354f00f0a23b4ab7b47afeacfccfda44ec38a47
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-11-04 23:52:17 +0100

    new sprint

--- src/Steps.js
+++ src/Steps.js
@@ -1,8 +1,31 @@
 import React from "react";
 import "./steps.css";
 
+const messages = ["Learn React", "Apply for Jobs", "Invest your new income"];
+
 const Steps = () => {
-  return <h3>Steps</h3>;
+  let step = 1;
+  return (
+    <div className='steps'>
+      <div className='numbers'>
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+      <p className='message'>
+        {" "}
+        Step {step}: {messages[0]}
+      </p>
+      <div className='buttons'>
+        <button style={{ backgroundColor: "#7950f2", color: "white" }}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Steps;
