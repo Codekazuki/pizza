8334ad29457300cee03259e3b244af63ad43dd61
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-11-05 13:35:37 +0100

    new sprint

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,25 +1,44 @@
 import React, { useState } from "react";
 
 const DateCounter = () => {
-  const [step, setStep] = useState(0);
+  const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
   const handleStepIncrease = () => {
     setStep((s) => s + 1);
   };
-
+  const handleStepDecrease = () => {
+    setStep((s) => s - 1);
+  };
+  const handleCountDecrease = () => {
+    setCount((c) => c - step);
+  };
+  const handleCountIncrease = () => {
+    setCount((c) => c + step);
+  };
+  const date = new Date();
+  date.setDate(date.getDate() + count);
   return (
     <div>
       <div>
-        <button onClick={handleStepIncrease}>-</button>
+        <button onClick={handleStepDecrease}>-</button>
         <span>step :{step}</span>
-        <button>+</button>
+        <button onClick={handleStepIncrease}>+</button>
       </div>
       <div>
-        <button>-</button>
+        <button onClick={handleCountDecrease}>-</button>
         <span>count:{count}</span>
-        <button>+</button>
+        <button onClick={handleCountIncrease}>+</button>
       </div>
-      <p>message</p>
+      <p>
+        <span>
+          {count === 0
+            ? "Today is"
+            : count > 0
+            ? `${count} days from now is `
+            : `${Math.abs(count)} days ago was`}
+        </span>
+        {date.toDateString()}
+      </p>
     </div>
   );
 };
