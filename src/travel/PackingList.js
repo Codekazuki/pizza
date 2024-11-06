4341c6193dd337f0b52392bc119fbb4cdc1be550
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-11-06 19:00:14 +0100

    new sprint

--- src/travel/PackingList.js
+++ src/travel/PackingList.js
@@ -4,11 +4,14 @@ import Item from "./Item";
 const PackingList = ({ items, onDeleteItem, onClearList, onToggleItem }) => {
   const [sortBy, setSortBy] = useState("input");
   let sortedItems;
+
   if (sortBy === "input") sortedItems = items;
+
   if (sortBy === "description")
     sortedItems = items
       .slice()
-      .sort((a, b) => a.desscription.localCompare(b.desscription));
+      .sort((a, b) => a.description.localeCompare(b.description));
+
   if (sortBy === "packed")
     sortedItems = items
       .slice()
