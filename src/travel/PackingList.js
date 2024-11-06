25e73f5b582c5d11458f98b6616ca79d96da6def
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-11-06 13:45:46 +0100

    new sprint

--- src/travel/PackingList.js
+++ src/travel/PackingList.js
@@ -1,9 +1,4 @@
 import React from "react";
-const initialItems = [
-  { id: 1, description: "passports", quantity: 2, packed: true },
-  { id: 2, description: "Socks", quantity: 3, packed: false },
-  { id: 3, description: "Ball", quantity: 1, packed: true },
-];
 
 const PackingList = ({ items, onDeleteItem, onClearList }) => {
   return (
@@ -28,7 +23,6 @@ const Item = ({ item, onDeleteItem }) => {
           item.packed ? { textDecoration: "line-through", color: "green" } : {}
         }
       >
-        {" "}
         {item.quantity} {item.description}
       </span>
       <button onClick={() => onDeleteItem(item.id)}>❌</button>
