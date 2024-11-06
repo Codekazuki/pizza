223019f9ac6843fa0c03d0ec1f357ff6cc067c18
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-11-06 07:53:03 +0100

    new sprint

--- src/travel/PackingList.js
+++ src/travel/PackingList.js
@@ -1,8 +1,8 @@
 import React from "react";
 const initialItems = [
-  { id: 1, description: "passports", quantity: 2, packed: false },
+  { id: 1, description: "passports", quantity: 2, packed: true },
   { id: 2, description: "Socks", quantity: 3, packed: false },
-  { id: 3, description: "Ball", quantity: 1, packed: false },
+  { id: 3, description: "Ball", quantity: 1, packed: true },
 ];
 
 const PackingList = () => {
@@ -22,11 +22,15 @@ export default PackingList;
 const Item = ({ item }) => {
   return (
     <li>
-      <span>
+      <span
+        style={
+          item.packed ? { textDecoration: "line-through", color: "green" } : {}
+        }
+      >
         {" "}
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      {item.packed ? <button>❎</button> : <button>❌</button>}
     </li>
   );
 };
