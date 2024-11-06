a0bd7cfa9190854bd6606158724feb27e47c1f9e
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-11-06 06:33:32 +0100

    new sprint

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Pizza from "./Pizza";
 import Steps from "./Steps";
 import DateCounter from "./DateCounter";
+import Travel from "./travel/Travel";
 
 const App = () => {
   return (
     <div>
-      <DateCounter />
+      <Travel />
+      {/* <DateCounter /> */}
       {/* <Steps /> */}
       {/* <Pizza /> */}
     </div>
---
+++ src/travel/Travel.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./travel.css";
+
+const Travel = () => {
+  return <div>Travel</div>;
+};
+
+export default Travel;
